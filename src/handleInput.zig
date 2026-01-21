65e5b5cead3187856679c38c91aa10efd43fcdf2
Brad Ben Joya <bradbenjoya6@gmail.com>
2026-01-21 19:01:13 +0200

    Added install command that clones a git repo.

--- src/handleInput.zig
+++ src/handleInput.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const handleInstall = @import("handleInstall.zig");
 const cpkgInit = @import("cpkgInit.zig");
 pub fn handleInput(args: []const []const u8) !void {
     if (args.len < 2) {
@@ -8,5 +9,8 @@ pub fn handleInput(args: []const []const u8) !void {
 
     if (std.mem.eql(u8, args[1], "init")) {
         try cpkgInit.cpkgInit();
+    }
+    if (std.mem.eql(u8, args[1], "install")) {
+        try handleInstall.handleInstall(args[2], args[3]);
     }
 }
--- src/handleInstall.zig
+++ src/handleInstall.zig
@@ -1,5 +1,39 @@
 const std = @import("std");
-pub fn handleInstall(package: []const []const u8, packageName: []const []const u8) !void {
-    const dir = try std.fs.cwd().openDir("cpkgExternal");
-        
+
+pub fn handleInstall(package: []const u8, packageName: []const u8) !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
+    var externalDir = try std.fs.cwd().openDir(
+        "cpkgExternal",
+        .{ .access_sub_paths = true },
+    );
+    defer externalDir.close();
+
+    try externalDir.makeDir(packageName);
+
+    var packageDir = try externalDir.openDir(
+        packageName,
+        .{ .access_sub_paths = true },
+    );
+    defer packageDir.close();
+
+    var child = std.process.Child.init(
+        &[_][]const u8{
+            "git",
+            "clone",
+            package,
+            ".",
+        },
+        allocator,
+    );
+
+    child.cwd_dir = packageDir;
+
+    const term = try child.spawnAndWait();
+    if (term != .Exited or term.Exited != 0) {
+        return error.GitCloneFailed;
+    }
 }
+
